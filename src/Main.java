88e2ebaf52b3f155230c2521f36994b72bd0808e
Charlotte-Miller <42000591+Charlotte-Miller@users.noreply.github.com>
2018-12-05 10:26:55 +0700

    Updating gameOver() method

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Game.addBird(BirdModel.THORD, 250);
-        Game.addBird(BirdModel.THORD, 250);
-        Game.addBird(BirdModel.THORD, 250);
-        Game.addBird(BirdModel.THORD, 250);
-        Game.addBird(BirdModel.THORD, 250);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
 
         Game.addMouse(MouseModel.MOUSELEFICENT, 225);
 
--- src/Model/BirdModel/BirdModel.java
+++ src/Model/BirdModel/BirdModel.java
@@ -6,10 +6,31 @@ import java.io.File;
 
 public final class BirdModel extends Model {
 
-    public static final String[] THORD =
+    public static final String[] THORD_ORIGINAL =
             {
-                    new File("src\\Model\\BirdModel\\Thord.png").getAbsolutePath()
+                    new File("src\\Model\\BirdModel\\Thord\\Thord_0.png").getAbsolutePath()
             };
+
+    public static final String[] THORD_HUE_1 =
+            {
+                    new File("src\\Model\\BirdModel\\Thord\\Thord_1.png").getAbsolutePath()
+            };
+
+    public static final String[] THORD_HUE_2 =
+            {
+                    new File("src\\Model\\BirdModel\\Thord\\Thord_2.png").getAbsolutePath()
+            };
+
+    public static final String[] THORD_HUE_3 =
+            {
+                    new File("src\\Model\\BirdModel\\Thord\\Thord_3.png").getAbsolutePath()
+            };
+
+    public static final String[] THORD_HUE_4 =
+            {
+                    new File("src\\Model\\BirdModel\\Thord\\Thord_4.png").getAbsolutePath()
+            };
+
 
     public static final String[] CHUCK =
             {
