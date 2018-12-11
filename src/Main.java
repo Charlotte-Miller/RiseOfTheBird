56b0410941ceea8093964d5a333a076581cfcef1
Charlotte-Miller <42000591+Charlotte-Miller@users.noreply.github.com>
2018-12-11 14:32:49 +0700

    Decreasing size of birds

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
-        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
-        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
-        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
-        Game.addBird(BirdModel.THORD_ORIGINAL, 300);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 200);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 200);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 200);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 200);
+        Game.addBird(BirdModel.THORD_ORIGINAL, 200);
 
         Game.addMouse(MouseModel.MOUSELEFICENT, 225);
 
